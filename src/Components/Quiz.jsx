bcee4839c169ac6799b5fadaf6198da79633e9f5
Shreejanlama <shreejanlama0@gmail.com>
2024-05-14 22:54:09 +0545

    check answer e.target haru use gareko xa

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Quiz.css'
+import { data } from '../assets/data'
 
 const Quiz = () => {
+   let [index, setIndex] = useState(1);
+   let [question, setQuestion] = useState(data[index]);
+  const checkAnswer = (e, answer) => {
+    if (question.answer === answer) {
+      e.target.classList.add('correct');
+    }
+    else {
+      e.target.classList.add('wrong');
+    }
+  }
+
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>1. which device is required for the internet connnection?
-        </h2>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Router</li>
-            <li>Modem</li>
-            <li>LAN Cable</li>
-            <li>Pen Drive</li>
+            <li onClick={(e)=>{checkAnswer(e,1)}}>{question.Option2}</li>
+            <li onClick={(e)=>{checkAnswer(e,2)}}>{question.Option1}</li>
+            <li onClick={(e)=>{checkAnswer(e,3)}}>{question.Option3}</li>
+            <li onClick={(e)=>{checkAnswer(e,4)}}>{question.Option4}</li>
         </ul>
         <button>Next</button>
         <div className='index'> 1 of 5 questions</div>
