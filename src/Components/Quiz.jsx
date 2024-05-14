034302560aa56f5425664a199b500d61ec8293cc
Shreejanlama <shreejanlama0@gmail.com>
2024-05-14 08:51:35 +0545

    first_commit

---
+++ src/Components/Quiz.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>which device is required for the internet connnection?
+        </h2>
+        <ul>
+            <li>Router</li>
+            <li>Modem</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className='index'> 1 of 5 question</div>
+    </div>
+  )
+}
+
+export default Quiz
