018eaaa34f66e8e0954ae14438ab45e6a1c0f4d1
Shreejanlama <shreejanlama0@gmail.com>
2024-05-15 23:54:52 +0545

    useRef used garya xa hamle

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
 import { data } from '../assets/data'
 
 const Quiz = () => {
    let [index, setIndex] = useState(1);
    let [question, setQuestion] = useState(data[index]);
-   let [locked, setLocked] = useState(false);
+   let [lock, setLock] = useState(false);
 
+   let Option1 = useRef(null);
+   let Option2 = useRef(null);
+   let Option3 = useRef(null);
+   let Option4 = useRef(null);
+
+   let option_array = [Option1, Option2, Option3, Option4];
   const checkAnswer = (e, answer) => {
     if (locl === false) {
       if (question.answer === answer) {
         e.target.classList.add('correct');
-        setLocked(true);
+        setLock(true);
       }
       else {
         e.target.classList.add('wrong');
+        setLock(true);
+        option_array[question.answer-1].current.classList.add('correct');
       }
       
     }
@@ -27,10 +35,10 @@ const Quiz = () => {
         <hr />
         <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li onClick={(e)=>{checkAnswer(e,1)}}>{question.Option2}</li>
-            <li onClick={(e)=>{checkAnswer(e,2)}}>{question.Option1}</li>
-            <li onClick={(e)=>{checkAnswer(e,3)}}>{question.Option3}</li>
-            <li onClick={(e)=>{checkAnswer(e,4)}}>{question.Option4}</li>
+            <li ref={Option1} onClick={(e)=>{checkAnswer(e,1)}}>{question.Option2}</li>
+            <li ref={Option2} onClick={(e)=>{checkAnswer(e,2)}}>{question.Option1}</li>
+            <li ref={Option3} onClick={(e)=>{checkAnswer(e,3)}}>{question.Option3}</li>
+            <li ref={Option4} onClick={(e)=>{checkAnswer(e,4)}}>{question.Option4}</li>
         </ul>
         <button>Next</button>
         <div className='index'> 1 of 5 questions</div>
